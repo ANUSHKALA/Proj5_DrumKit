1015acbecab6dcd54b43b3af4714641509df5f8c
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-03-04 13:45:19 +0530

    index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,13 +1,18 @@
 var n = document.querySelectorAll(".drum").length;
 for (let i = 0;i<n;i++){
-    document.querySelectorAll(".drum")[i].addEventListener('click',playSound)
+    document.querySelectorAll(".drum")[i].addEventListener('click',handleClick)
 }
 
 function handleClick() {
-    alert("Heyyyy there!!!")
+    this.style.color = "yellow";
 }
 
-var audio = new Audio("../Drum Kit Starting Files/sounds/tom-1.mp3");
-function playSound(){
-    audio.play();
-}
+
+
+
+
+
+// var audio = new Audio("../Drum Kit Starting Files/sounds/tom-1.mp3");
+// function playSound(){
+//     audio.play();
+// }
