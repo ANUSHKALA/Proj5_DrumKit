7458e487e85736b99d5a81f6df96c75fbb1d593b
ANUSHKALA <anushkakaimal2003@gmail.com>
2022-03-04 15:51:51 +0530

    sound on keypress

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -6,47 +6,51 @@ for (let i = 0;i<n;i++){
 function handleClick() {
 
     var clickedButton = this.innerHTML;
+    soundOnKey(clickedButton)
 
-    switch(clickedButton){
 
-        case("w"):
-            var audio = new Audio("../Drum Kit Starting Files/sounds/crash.mp3");
-            audio.play();
-        break;
-        case("a"):
-            var audio = new Audio("../Drum Kit Starting Files/sounds/kick-bass.mp3");
-            audio.play();
-        break;
-        case("s"):
-            var audio = new Audio("../Drum Kit Starting Files/sounds/snare.mp3");
-            audio.play();
-        break;
-        case("d"):
-            var audio = new Audio("../Drum Kit Starting Files/sounds/tom-1.mp3");
-            audio.play();
-        break;
-        case("j"):
-            var audio = new Audio("../Drum Kit Starting Files/sounds/tom-2.mp3");
-            audio.play();
-        break;
-        case("k"):
-            var audio = new Audio("../Drum Kit Starting Files/sounds/tom-3.mp3");
-            audio.play();
-        break;
-        case("l"):
-            var audio = new Audio("../Drum Kit Starting Files/sounds/tom-4.mp3");
-            audio.play();
-        break;
-        default:console.log(clickedButton)
-
-    }
 }
 
 
 
 
 
+document.addEventListener("keydown",function(event){soundOnKey(event.key)})
 
- function playSound(){
-     
- }
+function soundOnKey(key){
+
+
+    switch(key){
+
+        case("w"):
+            var audio = new Audio("../Drum Kit Starting Files/sounds/tom-1.mp3");
+            audio.play();
+        break;
+        case("a"):
+            var audio = new Audio("../Drum Kit Starting Files/sounds/tom-2.mp3");
+            audio.play();
+        break;
+        case("s"):
+            var audio = new Audio("../Drum Kit Starting Files/sounds/tom-3.mp3");
+            audio.play();
+        break;
+        case("d"):
+            var audio = new Audio("../Drum Kit Starting Files/sounds/tom-4.mp3");
+            audio.play();
+        break;
+        case("j"):
+            var audio = new Audio("../Drum Kit Starting Files/sounds/crash.mp3");
+            audio.play();
+        break;
+        case("k"):
+            var audio = new Audio("../Drum Kit Starting Files/sounds/kick-bass.mp3");
+            audio.play();
+        break;
+        case("l"):
+            var audio = new Audio("../Drum Kit Starting Files/sounds/snare.mp3");
+            audio.play();
+        break;
+        default:console.log(clickedButton)
+
+    }
+}
